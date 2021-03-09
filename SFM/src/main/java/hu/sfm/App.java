44e84a7305343e6fb9f034d132267c214d749f37
xjozsi <veresjozsef99@gmail.com>
2021-03-09 12:45:59 +0100

    működjél2

--- SFM/src/main/java/hu/sfm/App.java
+++ SFM/src/main/java/hu/sfm/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 651, 501);
+        scene = new Scene(loadFXML("primary"), 651, 502);
         stage.setScene(scene);
         stage.show();
     }
