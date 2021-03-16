a47e709467c606fc306e16a6488d7a300454c1bf
filepfannikiara <filepfannikiara@gmail.com>
2021-03-16 12:48:50 +0100

    Teszt0

--- SFM/src/main/java/hu/sfm/App.java
+++ SFM/src/main/java/hu/sfm/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 650, 400);
+        scene = new Scene(loadFXML("primary"), 651, 400);
         stage.setScene(scene);
         stage.show();
     }
