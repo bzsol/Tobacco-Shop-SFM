f2eefad57a440b0845892f18716595ba4aefda44
bzsol <61059824+bzsol@users.noreply.github.com>
2021-03-17 11:51:53 +0100

    Added ClassTest.java and added parameterized tests

---
+++ SFM/src/test/java/hu/sfm/ClassTest.java
@@ -0,0 +1,13 @@
+package hu.sfm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+public class ClassTest {
+
+}
