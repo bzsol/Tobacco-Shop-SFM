6ef2f65db811c75095f3e91fb6224eab1d644720
Ricsi <goozrichard@gmail.com>
2021-04-13 15:10:36 +0200

    in cart prices added

--- SFM/src/main/java/hu/sfm/controller/CartController.java
+++ SFM/src/main/java/hu/sfm/controller/CartController.java
@@ -1,6 +1,9 @@
 package hu.sfm.controller;
 
+import hu.sfm.entity.Product;
 import hu.sfm.main.Main;
+import hu.sfm.utils.JPAProductDAO;
+import hu.sfm.utils.ProductDAO;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -12,7 +15,10 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.persistence.TypedQuery;
 import java.util.Map;
+
+import static hu.sfm.utils.ProductDAO.entityManager;
 
 public class CartController {
     @FXML
@@ -54,7 +60,18 @@ public class CartController {
 
     private void setupCart() {
         int sorszam = 0;
+        String itemprice = "0";
+        ProductDAO productDAO = new JPAProductDAO();
+
         for (Map.Entry<String, Integer> product : Main.actualCart.entrySet()) {
+
+            for(Product p : productDAO.getProducts()){
+                if(p.getName().equals(product.getKey()))
+                {
+                    itemprice = String.valueOf(p.getPrice());
+                }
+            }
+
             HBox productLine = new HBox();
             productLine.setId(product.getKey());
             productLine.setMinHeight(50);
@@ -68,7 +85,7 @@ public class CartController {
             l1.setStyle("-fx-min-width: 90px; -fx-font-family: Segoe UI; -fx-text-fill: white; -fx-font-size: 16px; -fx-alignment: BASELINE_CENTER; -fx-label-padding: 15px 0px");
             Label l2 = new Label(product.getKey());
             l2.setStyle("-fx-min-width: 160px; -fx-font-family: Segoe UI; -fx-text-fill: white; -fx-font-size: 16px; -fx-alignment: BASELINE_CENTER; -fx-label-padding: 15px 0px");
-            Label l3 = new Label("Ft");
+            Label l3 = new Label((Integer.parseInt(itemprice)*product.getValue())+" Ft");
             l3.setStyle("-fx-min-width: 220px; -fx-font-family: Segoe UI; -fx-text-fill: white; -fx-font-size: 16px; -fx-alignment: BASELINE_CENTER; -fx-label-padding: 15px 0px 0px 140px");
             Button removeBtn = new Button("Eltávolítás");
             removeBtn.setStyle("-fx-background-color: transparent; -fx-border-color: red; -fx-border-width: 2px; -fx-border-radius: 50%; -fx-text-fill: white; -fx-font-family: Segoe UI; -fx-font-size: 14px; -fx-cursor: HAND");
